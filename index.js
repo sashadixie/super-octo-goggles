d5a1d70a6e4bb24dc0cb4c5c8213db0694c69c40
aleksandra.sokolova@rtlabs.ru <dixxiesasha@gmail.com>
2021-08-03 02:43:31 +0300

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function createHtmlChildElems(arr) {
   const list = document.createElement('div');
   list.innerHTML = '';
 
-  arr.forEach(function(element, index) {
+  arr.forEach(function (element, index) {
     const item = document.createElement('div');
     item.innerHTML = `результат: ${element}`
     list.appendChild(item);
@@ -10,6 +10,6 @@ function createHtmlChildElems(arr) {
   return list;
 }
 
-const foo = [4, 5, 666, -98, 45, 78];
+const foo = [4, 5, , , 666, -98, 45, 78, , , , 6, 6, 5, 4, 7];
 const elems = createHtmlChildElems(foo);
-document.getElementById('parent').append(elems);
+document.body.append(elems);
